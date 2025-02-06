3ea01ea42aadcd094dfea542a62ad7e5dc134147
Joe Chirchirillo <cchirchirillo3@gmail.com>
2025-02-05 22:28:24 -0700

    Chore: styling and organization

--- src/app/newaccount/page.tsx
+++ src/app/newaccount/page.tsx
@@ -1,6 +1,11 @@
 export default function NewAccountPage() {
   return (
-    <div>
+    <div
+    // flex flex-col items-center justify-center
+      className=" 
+      w-screen h-screen 
+      bg-[linear-gradient(45deg,_hsl(177,100%,50%)_0%,_hsl(177,99%,45%)_16%,_hsl(177,99%,39%)_25%,_hsl(177,99%,32%)_34%,_hsl(177,99%,22%)_48%,_hsl(0,0%,0%)_98%)]"
+    >
       <h1>New Account</h1>
     </div>
   );
